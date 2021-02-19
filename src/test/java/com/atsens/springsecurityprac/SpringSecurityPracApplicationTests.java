3937a39095615cd46c432ef0d28b130a5145f183
DahamLeee <dlek1011@gmail.com>
2021-02-19 16:21:08 +0900

    first commit

---
+++ src/test/java/com/atsens/springsecurityprac/SpringSecurityPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsens.springsecurityprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
