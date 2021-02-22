cfa4309a6ae256354c3c5375ea11207d7b585a5c
DahamLeee <dlek1011@gmail.com>
2021-02-22 09:49:05 +0900

    remember-me

--- src/main/java/com/atsens/springsecurityprac/config/SecurityConfig.java
+++ src/main/java/com/atsens/springsecurityprac/config/SecurityConfig.java
@@ -1,22 +1,30 @@
 package com.atsens.springsecurityprac.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,23 +39,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/login")
                 .usernameParameter("userId")
                 .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc")
+                .loginProcessingUrl("/login")
                 .successHandler(new AuthenticationSuccessHandler() {
                     @Override
                     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication " + authentication.getName());
+                        System.out.println("# Security Config : authentication " + authentication.getName());
                         response.sendRedirect("/");
                     }
                 })
                 .failureHandler(new AuthenticationFailureHandler() {
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                        System.out.println("exception " + exception.getMessage());
+                        System.out.println("# Security Config : exception " + exception.getMessage());
                         response.sendRedirect("/login");
                     }
                 })
                 .permitAll()
-
+        .and()
+                .rememberMe()
+                .rememberMeParameter("remember") // 기본 파라미터명은 remember-me
+                .tokenValiditySeconds(3600) // Default 는 14일
+                .alwaysRemember(false) // 리멤버 미 기능이 활성화되지 않아도 항상 실행
+                .userDetailsService(userDetailsService)
+        ;
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .deleteCookies("JSESSIONID", "remember-me")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/");
+                    }
+                })
         ;
     }
 }
--- src/test/java/com/atsens/springsecurityprac/SpringSecurityPracApplicationTests.java
+++ src/test/java/com/atsens/springsecurityprac/SpringSecurityPracApplicationTests.java
@@ -9,5 +9,4 @@ class SpringSecurityPracApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
